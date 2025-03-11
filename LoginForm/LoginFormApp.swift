2c77e31b73a7f792cb49b94935e367ed93a1acdf
anixp20101999 <animesh.rout@travelxp.tv>
2025-03-11 21:39:16 +0530

    Initial Commit

---
+++ LoginForm/LoginFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginFormApp.swift
+//  LoginForm
+//
+//  Created by Animesh Rout on 11/03/25.
+//
+
+import SwiftUI
+
+@main
+struct LoginFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LoginFormTests/LoginFormTests.swift
@@ -0,0 +1,17 @@
+//
+//  LoginFormTests.swift
+//  LoginFormTests
+//
+//  Created by Animesh Rout on 11/03/25.
+//
+
+import Testing
+@testable import LoginForm
+
+struct LoginFormTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
